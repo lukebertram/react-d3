a18a33c1fd75482d57efd95e2b4ec2333c78c954
Luke Bertram <luke@foundryinteractive.com>
2024-04-29 12:57:59 -0700

    add Bars chart component

---
+++ src/Chart/Bars.tsx
@@ -0,0 +1,37 @@
+import * as d3 from "d3";
+import { callAccessor } from "../utils/chartUtils";
+
+type BarsProps = {
+  data: Datum[];
+  keyAccessor: Accessor;
+  xAccessor: Accessor;
+  yAccessor: Accessor;
+  widthAccessor: Accessor;
+  heightAccessor: Accessor;
+  [x: string]: unknown;
+};
+
+const Bars = ({
+  data,
+  keyAccessor,
+  xAccessor,
+  yAccessor,
+  widthAccessor,
+  heightAccessor,
+  ...props
+}: BarsProps) => (
+  <>
+		{data.map(d, i) => (
+			<rect {...props}
+				className="Bars__rect"
+				key={keyAccessor(d, i)}
+				x={callAccessor(xAccessor, d, i)}
+				y={callAccessor(yAccessor, d, i)}
+				width={d3.max([callAccessor(widthAccessor, d, i), 0])}
+				height={d3.max([callAccessor(heightAccessor, d, i), 0])}
+			/>
+		)}
+	</>
+);
+
+export default Bars;
